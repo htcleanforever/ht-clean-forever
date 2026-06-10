3518254596c55583d124284ecdc0fa8a0a0afcfb
ht-clean-forever <saultrejogamer@gmail.com>
2026-06-09 21:37:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-// HT Clean Forever Scripts
+const images = document.querySelectorAll(".gallery-grid img");
+
+images.forEach(img => {
+    img.addEventListener("click", () => {
+
+        const overlay = document.createElement("div");
+        overlay.classList.add("lightbox");
+
+        const bigImage = document.createElement("img");
+        bigImage.src = img.src;
+
+        overlay.appendChild(bigImage);
+
+        document.body.appendChild(overlay);
+
+        overlay.addEventListener("click", () => {
+            overlay.remove();
+        });
+
+    });
+});
