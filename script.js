0d1922cc12f55c026666de2e12e68c0600c10cd6
ht-clean-forever <saultrejogamer@gmail.com>
2026-06-10 17:21:19 -0700

    Update script.js

--- script.js
+++ script.js
@@ -19,3 +19,9 @@ images.forEach(img => {
 
     });
 });
+const menuToggle = document.querySelector('.menu-toggle');
+const navLinks = document.querySelector('.nav-links');
+
+menuToggle.addEventListener('click', () => {
+    navLinks.classList.toggle('active');
+});
