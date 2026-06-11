a8c3d9ff453eac148040ff34c561e0820e782606
ht-clean-forever <saultrejogamer@gmail.com>
2026-06-10 19:30:58 -0700

    Update script.js

--- script.js
+++ script.js
@@ -25,3 +25,16 @@ const navLinks = document.querySelector('.nav-links');
 menuToggle.addEventListener('click', () => {
     navLinks.classList.toggle('active');
 });
+const fadeElements = document.querySelectorAll('.fade-in');
+
+const observer = new IntersectionObserver((entries) => {
+    entries.forEach(entry => {
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }
+    });
+});
+
+fadeElements.forEach(el => {
+    observer.observe(el);
+});
